f7cfc6ccee40321382b25b552a02a896b3cdf4da
Mark Watson <markwwork20@gmail.com>
2023-02-21 00:22:31 -0500

    Beginning js Row creation

---
+++ src/components/TierRow copy.js
@@ -0,0 +1,43 @@
+import "../pages/HomePage.css";
+import { Container, Row, Col } from "reactstrap";
+
+const TierRow = () => {
+  return (
+    <Container className="myContainer2">
+      <Row className="myRow">
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+        <Col>
+          <div className="myBoxP"></div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default TierRow;
--- src/components/TierRow.js
+++ src/components/TierRow.js
@@ -1,39 +1,26 @@
 import "../pages/HomePage.css";
 import { Container, Row, Col } from "reactstrap";
+import TierCell from "./TierCell";
+import StartGrid from "../app/shared/StartGrid";
 
 const TierRow = () => {
+  const makeCellRow = () => {
+    const currentRow = [...StartGrid[0].cellArray];
+    const tierArray = [];
+    for (let i = 1; i < 10; i++) {
+      tierArray.push(<TierCell />);
+      // console.log(currentRow[i]);
+    }
+    console.log(tierArray);
+    return tierArray;
+  };
+  makeCellRow();
+
   return (
     <Container className="myContainer2">
       <Row className="myRow">
         <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
-        </Col>
-        <Col>
-          <div className="myBoxP"></div>
+          <TierCell />
         </Col>
       </Row>
     </Container>
