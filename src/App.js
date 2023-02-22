dd9abbd0fe208919a0026a643a48fd60c03a001c
Mark Watson <markwwork20@gmail.com>
2023-02-21 21:57:51 -0500

    Added Header and IMAGEITEM

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import HomePage from "./pages/HomePage";
 import "./App.css";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <DndProvider backend={HTML5Backend}>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
